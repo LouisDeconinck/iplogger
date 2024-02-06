eaa5d1d39f8210345b2d09f52349488cc92371e1
Louis Deconinck <louis.dck@gmail.com>
2024-02-06 15:38:15 +0100

    Make build.sh executable

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Create Django superuser if CREATE_SUPERUSER environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
+  echo "Creating Django superuser..."
+  python iplogger/manage.py createsuperuser --no-input
+  echo "Django superuser created"
+fi
