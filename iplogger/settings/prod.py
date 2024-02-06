bf41beddf00857afbdc825f46ed3ee4758be15af
Louis Deconinck <louis.dck@gmail.com>
2024-02-06 15:46:57 +0100

    whitenoise

--- iplogger/settings/prod.py
+++ iplogger/settings/prod.py
@@ -14,3 +14,11 @@ if RENDER_EXTERNAL_HOSTNAME:
 DATABASES = {
 	"default": dj_database_url.parse(os.environ.get("DB_EXT_URL"))
 }
+
+# Tell Django to copy statics to the `staticfiles` directory
+# in your application directory on Render.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Turn on WhiteNoise storage backend that takes care of compressing static files
+# and creating unique names for each version so they can safely be cached forever.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
