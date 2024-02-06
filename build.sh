03752b98844a785943eb53f4086a542a017447a1
Louis Deconinck <louis.dck@gmail.com>
2024-02-06 15:53:31 +0100

    collectstatic

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # Install dependencies
 pip install -r requirements.txt
 
+python manage.py collectstatic --no-input
+
 # Create Django superuser if CREATE_SUPERUSER environment variable is set
 if [[ $CREATE_SUPERUSER ]]; then
   echo "Creating Django superuser..."
