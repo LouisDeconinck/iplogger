870f49a249540a3de7f68c53f17a88729e79ad65
Louis Deconinck <louis.dck@gmail.com>
2024-02-06 16:03:51 +0100

    superuser

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Create Django superuser if CREATE_SUPERUSER environment variable is set
-if [[ $CREATE_SUPERUSER ]]; then
-  echo "Creating Django superuser..."
-  python iplogger/manage.py createsuperuser --no-input
-  echo "Django superuser created"
-fi
+
+echo "Creating Django superuser..."
+python manage.py createsuperuser --no-input
+echo "Django superuser created"
