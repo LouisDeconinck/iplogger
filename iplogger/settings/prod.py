836b6f60e119b6ead99e349a88587b7d4f064eeb
Louis Deconinck <louis.dck@gmail.com>
2024-02-06 15:16:31 +0100

    render allowed hosts

--- iplogger/settings/prod.py
+++ iplogger/settings/prod.py
@@ -5,7 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 DATABASES = {
 	"default": dj_database_url.parse(os.environ.get("DB_EXT_URL"))
